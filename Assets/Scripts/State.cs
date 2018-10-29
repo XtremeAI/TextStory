f9feac37063952d6767450349b135a6aef119e2b
XtremeAI <40867886+XtremeAI@users.noreply.github.com>
2018-10-29 18:45:40 +0100

    Pre transfer

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+	[SerializeField, TextArea(10,14)] string _storyText;
+	[SerializeField] State[] _nextStates;
+
+	public string GetStoryState(){
+		return _storyText;
+	}
+
+	public State[] GetNextStates(){
+		return _nextStates;
+	}
+}
---
+++ Assets/Scripts/StoryScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryScript : MonoBehaviour {
+
+	[SerializeField] Text _storyText;
+	[SerializeField] State _startingState;
+
+	State _storyState;
+	// Use this for initialization
+	void Start () {
+		_storyState = _startingState;
+		_storyText.text = _storyState.GetStoryState();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ManageState();
+	}
+
+  private void ManageState()
+  {
+    var nextStates = _storyState.GetNextStates();
+		if (Input.GetKeyDown(KeyCode.Alpha1)){
+			_storyState = nextStates[0];
+			_storyText.text = _storyState.GetStoryState();
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2)){
+			_storyState = nextStates[1];
+			_storyText.text = _storyState.GetStoryState();
+		}
+  }
+}
